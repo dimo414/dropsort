055c258dde58bfc99d627bdc8300807ba6b8d905
Michael Diamond@Archer <dimo414@gmail.com>
2017-01-04 09:06:16 +0000

    Add util_test.py

--- util.py
+++ util.py
@@ -6,7 +6,8 @@ def partial_shuffle_orig(ls, randomness):
   """Approximate reimplementation of the algorithm in
   https://bitbucket.org/dimo414/dropsort/src/pristine/Research/DropsortTest/Dropsort.cs?t#Dropsort.cs-25
 
-  Note that it can introduce duplicates.
+  Note that it does not necessarily preserve the contents of the original array
+  (as it selects with replacement, introducing duplicate and dropped values).
   """
   copy = ls[:]
   for i in range(len(copy)):
@@ -20,7 +21,7 @@ def partial_shuffle_sample(ls, randomness):
   effectively round-down to zero (a subset of size zero or one cannot be
   shuffled).
   """
-  num_shuffle = math.ceil(randomness * len(ls))
+  num_shuffle = int(math.ceil(randomness * len(ls)))
   print(num_shuffle)
   indicies = random.sample(range(len(ls)), num_shuffle)
   print(indicies)
@@ -32,10 +33,11 @@ def partial_shuffle_sample(ls, randomness):
     copy[indicies_shuffled[i]] = ls[indicies[i]]
   return copy
 
-def partial_shuffle(ls, randomness):
+def partial_shuffle(ls, randomness, reverse=True):
   """Partially shuffles a list by applying the Fisher-Yates shuffle with a
-  given probability."""
-  copy = ls[:] if random.getrandbits(1) else ls[::-1]
+  given probability. By default also reverses the result 50% of the time to
+  avoid directional bias."""
+  copy = ls[:] if reverse and random.getrandbits(1) else ls[::-1]
   for i in range(len(copy)):
     if random.random() < randomness:
       j = random.choice(range(i, len(copy)))
@@ -45,30 +47,17 @@ def partial_shuffle(ls, randomness):
         copy[j] = tmp
   return copy
 
-def partial_shuffle_and_reverse(ls, randomness):
-  """Invokes partial_shuffle() and, 50% of the time reverses the list, to
-  avoid directional bias."""
-  ls = partial_shuffle(ls, randomness)
-  if random.getrandbits(1):
-    reversed(ls)
-  return ls
-
 def is_sorted(ls):
   """Returns true if the list is sorted"""
   return ls == sorted(ls)
 
-def verify(f, size=100):
-  """Verifies that f returns a sorted list when passed a possibly-unsorted one.
-  """
-  def check_sorted(ls):
-    if not is_sorted(f(ls)):
-      raise AssertionError("%s didn't dropsort %s - returned %s" % (
-          f.__name__, ls, f(ls)))
-  ls = sorted(random.sample(range(size * 10), size))
-  check_sorted(ls) # ordered
-  check_sorted(ls[::-1]) # reversed
-  for _ in range(100):
-    random.shuffle(ls)
-    check_sorted(ls)
-    
-
+def is_sublist(ls, subls):
+  """Returns true if every element of subls exists, in order, in ls."""
+  if len(ls) < len(subls):
+    return False
+  ls_i, subls_i = 0, 0
+  while ls_i < len(ls) and subls_i < len(subls):
+    if ls[ls_i] == subls[subls_i]:
+      ls_i += 1
+    subls_i +=1
+  return subls_i == len(subls)
